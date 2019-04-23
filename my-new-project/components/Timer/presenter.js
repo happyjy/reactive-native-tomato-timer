33c3ff2b0f873c0f2876518073f7b79b08a8232f
happyjy <okwoyjy@gmail.com>
2019-04-23 23:52:35 +0900

    MODIFIED CSS  MIN,SECOND POSITION

--- my-new-project/components/Timer/presenter.js
+++ my-new-project/components/Timer/presenter.js
@@ -6,7 +6,7 @@ function formatTime(time){
   let minutes = Math.floor(time / 60);
   time -= minutes * 60;
   let seconds = parseInt(time % 60, 10);
-  return `${minutes < 10 ? `0${minutes}` : minutes}: ${seconds < 10 ? `0${seconds}`: seconds}`;
+  return `${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}`: seconds}`;
 }
 
 class Timer extends Component {
@@ -41,12 +41,11 @@ class Timer extends Component {
       elapsedTime,
       timerDuration,
       startTimer,
-      restartTimer,
-      addSecond
+      restartTimer
     } = this.props;
     return (
       <View style={styles.container}>
-        <StatusBar barStyle={"light-content"} />
+        <StatusBar barStyle={"light-content"}/>
         <View style={styles.upper}>
           <Text style={styles.time}> 
            {formatTime(timerDuration - elapsedTime)}
@@ -69,19 +68,23 @@ class Timer extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#bc0b24'
+    backgroundColor: "#CE0B24"
+  },
+  lower: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+    paddingLeft: 25,
+    paddingRight: 25
   },
   upper: {
     flex: 2,
     justifyContent: "center",
-  },
-  lower: {
-    flex: 1,
-    justifyContent: "center",
     alignItems: "center"
   },
   time: {
-    color: 'white',
+    color: "white",
     fontSize: 120,
     fontWeight: "100"
   }
